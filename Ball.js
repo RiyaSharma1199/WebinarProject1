77c7833e17ae7db557ec31a2fa43573339ed2a10
Riya Sharma <74506498+RiyaSharma1199@users.noreply.github.com>
2020-12-11 22:25:34 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,30 @@
+class Ball {
+    constructor(x, y) {
+      var options = {
+        restitution: 1,
+        friction: 0.5,
+        frictionAir: 0.0,
+        slop: 1,
+        inertia: Infinity
+    };
+    this.body = Bodies.rectangle(x, y, 40, 40, options);
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    World.add(world, this.body);
+  }
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('white')
+      fill('lightblue')
+      ellipseMode(RADIUS)
+      ellipse(0, 0, 20,20);
+      pop();
+    };
+  };
